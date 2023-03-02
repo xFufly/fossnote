470d994e00a427786c4f762b12473de2d4d3c3b7
Fufly <caraploof@gmail.com>
2023-03-02 16:33:37 +0100

    Beautify code

--- src/databases/eleves.js
+++ src/databases/eleves.js
@@ -1,15 +1,15 @@
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database('database.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    console.log('Connected to the database.');
-  }
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to the database.');
+    }
 });
 
 db.serialize(() => {
-  const table = `CREATE TABLE IF NOT EXISTS students (
+    const table = `CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nom TEXT,
     prenom TEXT,
@@ -19,92 +19,96 @@ db.serialize(() => {
     username TEXT UNIQUE,
     password TEXT
   )`;
-  db.run(table, (err) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      console.log('Table "students" init.');
-    }
-  });
-  
-  const exampleUser = `INSERT INTO students (nom, prenom, usertype, classe, groupes, username, password)
+    db.run(table, (err) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log('Table "students" init.');
+        }
+    });
+
+    const exampleUser = `INSERT INTO students (nom, prenom, usertype, classe, groupes, username, password)
   SELECT 'KATY', 'Alex', 3, '3A', 'groupe1,groupe2', 'akaty', 'Password123!'
   WHERE NOT EXISTS (SELECT 1 FROM students WHERE username = 'TDIDE')`;
-  db.run(exampleUser, (err) => {
-    if (err) {
-      console.error(err.message);
-    } else {
-      console.log('Example user created.');
-    }
-  });
+    db.run(exampleUser, (err) => {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log('Example user created.');
+        }
+    });
 });
 
 // Fonction pour créer un nouvel utilisateur
 async function createUser(nom, prenom, usertype, classe, groupes, ids) {
-  const insert = `INSERT INTO students (nom, prenom, usertype, classe, groupes, username, password)
+    const insert = `INSERT INTO students (nom, prenom, usertype, classe, groupes, username, password)
     VALUES (?, ?, ?, ?, ?, ?, ?)`;
-  const values = [nom, prenom, usertype, classe, groupes.join(','), ids.username, ids.password];
-  return new Promise((resolve, reject) => {
-    db.run(insert, values, function(err) {
-      if (err) {
-        reject(err);
-      } else {
-        const user = {
-          id: this.lastID,
-          nom,
-          prenom,
-          usertype,
-          classe,
-          groupes,
-          ids
-        };
-        resolve(user);
-      }
+    const values = [nom, prenom, usertype, classe, groupes.join(','), ids.username, ids.password];
+    return new Promise((resolve, reject) => {
+        db.run(insert, values, function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                const user = {
+                    id: this.lastID,
+                    nom,
+                    prenom,
+                    usertype,
+                    classe,
+                    groupes,
+                    ids
+                };
+                resolve(user);
+            }
+        });
     });
-  });
 }
 
 // Fonction pour récupérer un utilisateur existant par son identifiant
 async function getUser(id) {
-  const select = `SELECT * FROM students WHERE username = ?`;
-  const values = [id];
-  return new Promise((resolve, reject) => {
-    db.get(select, values, (err, row) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(row ? {
-          id: row.id,
-          nom: row.nom,
-          prenom: row.prenom,
-          usertype: row.usertype,
-          classe: row.classe,
-          groupes: row.groupes.split(','),
-          ids: {
-            username: row.username,
-            password: row.password
-          }
-        } : null);
-      }
+    const select = `SELECT * FROM students WHERE username = ?`;
+    const values = [id];
+    return new Promise((resolve, reject) => {
+        db.get(select, values, (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row ? {
+                    id: row.id,
+                    nom: row.nom,
+                    prenom: row.prenom,
+                    usertype: row.usertype,
+                    classe: row.classe,
+                    groupes: row.groupes.split(','),
+                    ids: {
+                        username: row.username,
+                        password: row.password
+                    }
+                } : null);
+            }
+        });
     });
-  });
 }
 
 // Fonction pour mettre à jour le mot de passe d'un utilisateur
 async function setPassword(id, password) {
-  const update = `UPDATE students SET password = ? WHERE username = ?`;
-  const values = [password, id];
-  return new Promise((resolve, reject) => {
-    db.run(update, values, function(err) {
-      if (err) {
-        reject(err);
-      } else if (this.changes === 1) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
+    const update = `UPDATE students SET password = ? WHERE username = ?`;
+    const values = [password, id];
+    return new Promise((resolve, reject) => {
+        db.run(update, values, function(err) {
+            if (err) {
+                reject(err);
+            } else if (this.changes === 1) {
+                resolve(true);
+            } else {
+                resolve(false);
+            }
+        });
     });
-  });
 }
 
-module.exports = { createUser, getUser, setPassword };
+module.exports = {
+    createUser,
+    getUser,
+    setPassword
+};
--- src/databases/session.js
+++ src/databases/session.js
@@ -1,16 +1,16 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  } else {
-    console.log('Connected to the "session" database.');
-  }
+    if (err) {
+        console.error(err.message);
+    } else {
+        console.log('Connected to the "session" database.');
+    }
 });
 const SESSION_DURATION = 10 * 60 * 1000; // 10 minutes
 
 // Fonction pour créer la table "sessions"
 function createSessionsTable() {
-  db.run(`CREATE TABLE IF NOT EXISTS sessions (
+    db.run(`CREATE TABLE IF NOT EXISTS sessions (
     _id TEXT PRIMARY KEY,
     numeroOrdre INTEGER,
     session_params TEXT,
@@ -22,68 +22,68 @@ function createSessionsTable() {
 }
 
 async function createSession(session_id, numeroOrdre, session_params, privateKeyPem, aes, challenge) {
-  db.run(`DELETE FROM sessions WHERE expiresAt <= ?`, Date.now());
-  const expiresAt = Date.now() + SESSION_DURATION;
-  const stmt = db.prepare(`INSERT INTO sessions (_id, numeroOrdre, session_params, privateKeyPem, expiresAt, aes, challenge) VALUES (?, ?, ?, ?, ?, ?, ?)`);
-  stmt.run(session_id, numeroOrdre, JSON.stringify(session_params), privateKeyPem, expiresAt, JSON.stringify(aes), JSON.stringify(challenge));
-  stmt.finalize();
-  return session_id;
+    db.run(`DELETE FROM sessions WHERE expiresAt <= ?`, Date.now());
+    const expiresAt = Date.now() + SESSION_DURATION;
+    const stmt = db.prepare(`INSERT INTO sessions (_id, numeroOrdre, session_params, privateKeyPem, expiresAt, aes, challenge) VALUES (?, ?, ?, ?, ?, ?, ?)`);
+    stmt.run(session_id, numeroOrdre, JSON.stringify(session_params), privateKeyPem, expiresAt, JSON.stringify(aes), JSON.stringify(challenge));
+    stmt.finalize();
+    return session_id;
 }
 
 async function getSession(session_id) {
-  const session = await new Promise((resolve, reject) => {
-    db.get(`SELECT * FROM sessions WHERE _id = ? AND expiresAt > ?`, session_id, Date.now(), (err, row) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(row);
-      }
+    const session = await new Promise((resolve, reject) => {
+        db.get(`SELECT * FROM sessions WHERE _id = ? AND expiresAt > ?`, session_id, Date.now(), (err, row) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(row);
+            }
+        });
     });
-  });
-  await new Promise(resolve => setTimeout(resolve, 10)); // pause de 10 ms
-  return await session;
+    await new Promise(resolve => setTimeout(resolve, 10)); // pause de 10 ms
+    return await session;
 }
 
 async function setAesSession(session_id, aes) {
-  const session = await getSession(session_id);
-  if (session) {
-    const stmt = db.prepare(`UPDATE sessions SET aes = ? WHERE _id = ?`);
-    stmt.run(JSON.stringify(aes), session_id);
-    stmt.finalize();
-    return true;
-  }
-  return false;
+    const session = await getSession(session_id);
+    if (session) {
+        const stmt = db.prepare(`UPDATE sessions SET aes = ? WHERE _id = ?`);
+        stmt.run(JSON.stringify(aes), session_id);
+        stmt.finalize();
+        return true;
+    }
+    return false;
 }
 
 async function setChallengeSession(session_id, challenge) {
-  const session = await getSession(session_id);
-  if (session) {
-    const stmt = db.prepare(`UPDATE sessions SET challenge = ? WHERE _id = ?`);
-    stmt.run(JSON.stringify(challenge), session_id);
-    stmt.finalize();
-    return true;
-  }
-  return false;
+    const session = await getSession(session_id);
+    if (session) {
+        const stmt = db.prepare(`UPDATE sessions SET challenge = ? WHERE _id = ?`);
+        stmt.run(JSON.stringify(challenge), session_id);
+        stmt.finalize();
+        return true;
+    }
+    return false;
 }
 
 async function setNumeroOrdreSession(numeroOrdre, session_id) {
-  const session = await getSession(session_id);
-  if (session && session.expiresAt > Date.now()) {
-    const stmt = db.prepare(`UPDATE sessions SET numeroOrdre = ? WHERE _id = ?`);
-    stmt.run(numeroOrdre, session_id);
-    stmt.finalize();
-    return true;
-  }
-  return false;
+    const session = await getSession(session_id);
+    if (session && session.expiresAt > Date.now()) {
+        const stmt = db.prepare(`UPDATE sessions SET numeroOrdre = ? WHERE _id = ?`);
+        stmt.run(numeroOrdre, session_id);
+        stmt.finalize();
+        return true;
+    }
+    return false;
 }
 
 // Appel de la fonction pour créer la table "sessions"
 createSessionsTable();
 
 module.exports = {
-  createSession,
-  getSession,
-  setAesSession,
-  setNumeroOrdreSession,
-  setChallengeSession
+    createSession,
+    getSession,
+    setAesSession,
+    setNumeroOrdreSession,
+    setChallengeSession
 };
